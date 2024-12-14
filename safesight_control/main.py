207be460a13345d6da71b0a11f2adb766338a5ea
vimzoomer <pelczarskik@student.agh.edu.pl>
2024-12-14 15:36:38 +0100

    interface class basic sockets

--- safesight_control/main.py
+++ safesight_control/main.py
@@ -1,10 +1,22 @@
 from src.gui import Gui
-
+from src import interface
 
 def main():
     gui = Gui()
     gui.setup()
 
-
 if __name__ == "__main__":
-    main()
+    interface = interface.Interface()
+    ip = "192.168.4.1"
+    port = 10000
+
+    try:
+        interface.connect(ip, port)
+        interface.send_message("Hello, ESP32!")
+        response = interface.receive_message()
+        if response:
+            print(f"Response from server: {response}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        interface.disconnect()
--- safesight_control/src/interface.py
+++ safesight_control/src/interface.py
@@ -1,3 +1,30 @@
+import socket
+
 class Interface:
     def __init__(self):
-        ...
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self, ip, port):
+        self.client_socket.connect((ip, port))
+
+    def disconnect(self):
+        self.client_socket.close()
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send_message(self, message):
+        try:
+            self.client_socket.sendall(message.encode('utf-8'))
+        except socket.error as e:
+            print(f"Error sending message: {e}")
+
+    def receive_message(self, buffer_size=1024):
+        try:
+            response = self.client_socket.recv(buffer_size)
+            message = response.decode('utf-8')
+            return message
+        except socket.error as e:
+            return None
+
+
+    
+
