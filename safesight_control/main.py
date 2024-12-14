87ab53af9f5f8adfbbde2c53792ab2e7822de942
vimzoomer <pelczarskik@student.agh.edu.pl>
2024-12-14 15:44:18 +0100

    usuniecie testow z main

--- safesight_control/main.py
+++ safesight_control/main.py
@@ -6,17 +6,4 @@ def main():
     gui.setup()
 
 if __name__ == "__main__":
-    interface = interface.Interface()
-    ip = "192.168.4.1"
-    port = 10000
-
-    try:
-        interface.connect(ip, port)
-        interface.send_message("Hello, ESP32!")
-        response = interface.receive_message()
-        if response:
-            print(f"Response from server: {response}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        interface.disconnect()
+    main()
